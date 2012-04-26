e98fcb11262216249912b9808bfe8b4f65b7cd88
Fletcher Fowler <fletch@fzf.me>
2012-04-25 18:38:59 -0700

    removing stuff that is not covered by tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
   before_filter :require_user, only: [:dashboard]
   respond_to :json
   
-  def new
-    @identity = env['omniauth.identity']
-  end
-  
   def create
     @provider = Provider.find_or_create params[:provider][:type], params[:provider][:token]
     
@@ -14,29 +10,5 @@ class UsersController < ApplicationController
       @user.save
     end
     render json: {user_id: @provider.user.id, provider_id: @provider.id}
-  end
-  
-  def forgot_password
-    if params[:email]
-      @user = User.where(email: params[:email]).first
-      if @user
-        @user.reset_password
-      else
-        redirect_to :back, alert: 'Email address not found'
-      end
-    end
-  end
-  
-  def reset_password
-    @user = User.where(token: params[:token]).first
-  end
-  
-  def update_password
-    @user = User.find params[:user_id]
-    if @user.update_attributes params[:user]
-      redirect_to login_path
-    else
-      redirect_to reset_password_path(token: @user.token), alert: 'Passwords do not match'
-    end
   end
 end
